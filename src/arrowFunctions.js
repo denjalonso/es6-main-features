5f87ab170438886984ea0f91243998039cadc6ee
Denis Juan Alonso Glez <denjalonso@gmail.com>
2016-04-13 17:01:52 +0100

    arrow function as parameter example

--- src/arrowFunctions.js
+++ src/arrowFunctions.js
@@ -5,3 +5,10 @@ export let greetingArrowFirstSintax = (message, name) => {
 export let greetingArrowSecondSintax = (message, name) => message + ' ' + name;
 
 export let greetingArrowSecondSintaxOneParameter = name => 'Hello ' + name;
+
+export let sumOfArrayNumbers =  (array) => {
+    let total = 0;
+    array.forEach(item => total += item)
+    
+    return total;
+};
