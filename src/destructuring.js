88b642dffed6ff2e0854fe39a79aeadff03c6a4a
Denis Juan Alonso Glez <denjalonso@gmail.com>
2016-04-13 12:52:44 +0100

    functions with multiple return values

--- src/destructuring.js
+++ src/destructuring.js
@@ -2,3 +2,11 @@
 let  colors = ["red", "green", "blue", "yellow", "orange"];
 
 export let [primary, secondary, tertiary, ...otherColors] = colors;
+
+function getDate() {
+    let currentDay = new Date();
+
+    return [currentDay.getDate(), currentDay.getMonth() + 1, currentDay.getFullYear()]
+}
+
+export var [day, month, year] = getDate();
--- tests/destructuring.spec.js
+++ tests/destructuring.spec.js
@@ -1,4 +1,4 @@
-import {primary, secondary, tertiary, otherColors} from "../src/destructuring";
+import {primary, secondary, tertiary, otherColors, day, month, year} from "../src/destructuring";
 
 describe("destructuring", () => {
     describe("creating variables from array elements", () => {
@@ -12,6 +12,14 @@ describe("destructuring", () => {
         it("should spread the last parameter as an array that contains the rest o values", () => {
             expect(otherColors).toBeAn(Array);
             expect(otherColors).toEqual(["yellow", "orange"]);
+        });
+    });
+    describe("functions with multiple return values", () => {
+        it("should return an array of values", () => {
+            var currentDay = new Date();
+            expect(day).toEqual(currentDay.getDate());
+            expect(month).toEqual(currentDay.getMonth() + 1);
+            expect(year).toEqual(currentDay.getFullYear());
         });
     });
 });
